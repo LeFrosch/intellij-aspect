b2514b8b88979c2b5e714eccb8cf793458613940
github-action-benchmark <github@users.noreply.github.com>
2026-09-10 14:22:40 +0000

    add Memory Overhead (customSmallerIsBetter) benchmark result for 851310f9f4d688b6c0daee4b29894cbd54d84b6d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789035084804,
+  "lastUpdate": 1789050160329,
   "repoUrl": "https://github.com/LeFrosch/intellij-aspect",
   "entries": {
     "Heap Analysis": [
@@ -133,6 +133,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "bazel:analysis_heap_committed",
             "value": 103.05555555555554,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "daniel.brauner@jetbrains.com",
+            "name": "Daniel Brauner",
+            "username": "LeFrosch"
+          },
+          "committer": {
+            "email": "daniel.brauner@jetbrains.com",
+            "name": "Daniel Brauner",
+            "username": "LeFrosch"
+          },
+          "distinct": true,
+          "id": "851310f9f4d688b6c0daee4b29894cbd54d84b6d",
+          "message": "WIP",
+          "timestamp": "2026-09-10T15:35:23+02:00",
+          "tree_id": "101c080a3d3f0a021d50929aa6a2a01e06720b00",
+          "url": "https://github.com/LeFrosch/intellij-aspect/commit/851310f9f4d688b6c0daee4b29894cbd54d84b6d"
+        },
+        "date": 1789050158674,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bazel:used-heap-size-after-gc",
+            "value": 120.54794520547945,
+            "unit": "%"
+          },
+          {
+            "name": "bazel:peak-heap-size",
+            "value": 77.09497206703911,
+            "unit": "%"
+          },
+          {
+            "name": "bazel:analysis_heap_used",
+            "value": 119.68558564676056,
+            "unit": "%"
+          },
+          {
+            "name": "bazel:analysis_heap_committed",
+            "value": 114.16666666666666,
+            "unit": "%"
+          },
+          {
+            "name": "pigweed:used-heap-size-after-gc",
+            "value": 38.32684824902724,
+            "unit": "%"
+          },
+          {
+            "name": "pigweed:peak-heap-size",
+            "value": 32.75590551181102,
+            "unit": "%"
+          },
+          {
+            "name": "pigweed:analysis_heap_used",
+            "value": 39.53297488830181,
+            "unit": "%"
+          },
+          {
+            "name": "pigweed:analysis_heap_committed",
+            "value": 21.644498186215237,
             "unit": "%"
           }
         ]
